70d3f14499ecee82b2bb1b0b99aa019d711f5166
Vorondanil54 <voronchihin.danill@yandex.ru>
2026-03-25 23:33:50 +0700

    Made greeting sequence

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+function greet() {
+    const username = readlineSync.question('May I know your name? ')
+    console.log(`Hello, ${username}!`)
+}
+
+export { greet }
