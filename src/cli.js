575d357f99959bd8ccf306b50498ffd83217a05b
Vorondanil54 <voronchihin.danill@yandex.ru>
2026-03-26 12:45:51 +0700

    Made "brain-even" minigame

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync'
 function greet() {
     const username = readlineSync.question('May I know your name? ')
     console.log(`Hello, ${username}!`)
+    return username
 }
 
 export { greet }
