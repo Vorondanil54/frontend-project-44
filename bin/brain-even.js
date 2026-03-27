99acedb77f2630e77026c31c4089ea00c4d4cbc6
Vorondanil54 <voronchihin.danill@yandex.ru>
2026-03-27 20:28:51 +0700

    Linted brain-even.js and cli.js files' trailing spaces

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -11,20 +11,20 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".')
 let answerWrong = false
 
 for (let count = 1; count < 4; count += 1) {
-    const question = Math.floor(Math.random() * (20 - 1 + 1)) + 1
-    console.log(`Question: ${question}`)
+  const question = Math.floor(Math.random() * (20 - 1 + 1)) + 1
+  console.log(`Question: ${question}`)
 
-    const userAnswer = readlineSync.question('Your answer: ')
-    const systemAnswer = question % 2 === 0 ? 'yes' : 'no'
+  const userAnswer = readlineSync.question('Your answer: ')
+  const systemAnswer = question % 2 === 0 ? 'yes' : 'no'
 
-    if (userAnswer === systemAnswer) {
-        console.log('Correct!')
-    }
-    else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer is '${systemAnswer}'.`)
-        answerWrong = true
-        break
-    }
+  if (userAnswer === systemAnswer) {
+    console.log('Correct!')
+  }
+  else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer is '${systemAnswer}'.`)
+    answerWrong = true
+    break
+  }
 }
 
 answerWrong === true ? console.log(`Let's try again, ${username}!`) : console.log(`Congratulations, ${username}!`)
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync'
 
 function greet() {
-    const username = readlineSync.question('May I know your name? ')
-    console.log(`Hello, ${username}!`)
-    return username
+  const username = readlineSync.question('May I know your name? ')
+  console.log(`Hello, ${username}!`)
+  return username
 }
 
 export { greet }
