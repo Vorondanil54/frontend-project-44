5795919f2afce0499c7556c8e3101427b8f4e0aa
Vorondanil54 <voronchihin.danill@yandex.ru>
2026-04-03 13:59:21 +0700

    Made "brain-prime" minigame

---
+++ games/brain-prime.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync'
+import greet from '../src/cli.js'
+
+const username = greet()
+
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+let answerWrong = false
+
+for (let count = 1; count <= 3; count += 1) {
+  function isPrime(num) {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) return false
+    }
+    return num !== 1
+  }
+
+  const question = Math.floor(Math.random() * (100 - 1 + 1)) + 1
+  console.log(`Question: ${question}`)
+
+  const userAnswer = readlineSync.question('Your answer: ')
+  const systemAnswer = isPrime(question) === true ? 'yes' : 'no'
+
+  if (userAnswer === systemAnswer) {
+    console.log('Correct!')
+  }
+  else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer is '${systemAnswer}'.`)
+    answerWrong = true
+    break
+  }
+}
+
+answerWrong === true ? console.log(`Let's try again, ${username}!`) : console.log(`Congratulations, ${username}!`)
